d5b0b834b592f44a03010fbfee2685d3148d938e
HimanshuBohra <39338459+HimanshuBohra@users.noreply.github.com>
2018-12-26 18:29:16 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import discord
 
+client = discord.Client() 
+@client.event
+async def on_ready():
+  print("hi")
+
+@client.event
+async def on_message(message):
+  a = message.content
+  if message.author != client.user:
+    if 'hi' in a:
+       await client.send_message(message.channel, "suprise muthafuka!!")
+    
+client.run("NTI3NDU0OTYyODE3NDMzNjAx.DwT_Ow.lfLZdJXX8FXthJrSy_TYsAtTDEc")
